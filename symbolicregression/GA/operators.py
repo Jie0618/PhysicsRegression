74ce445e4b7de81c8dc8a321234405cef6e4c64b
Jie0618 <2295872723@qq.com>
2025-04-23 20:27:04 +0800

    refactor source code of GA

---
+++ symbolicregression/GA/operators.py
@@ -0,0 +1,36 @@
+BINARY_OP = {
+    "add": 1,
+    "sub": 1,
+    "mul": 1, 
+    #"div": 1,
+    #"pow": 1,
+}
+
+UNARY_OP = {
+    #"abs": 1,
+    "inv": 1,
+    "sqrt": 1,
+
+    #"log": 1,
+    "exp": 1,
+
+    "sin": 1,
+    #"arcsin": 1,
+    "cos": 1,
+    #"arccos": 1,
+    "tan": 1,
+    #"arctan": 1,
+
+    "pow2": 1,
+    "pow3": 1,
+
+    "neg": 1,
+}
+
+SKELETON_OP = {
+    "skeleton": 1
+}
+
+SETTING_OP = {
+    "empty": 1
+}
